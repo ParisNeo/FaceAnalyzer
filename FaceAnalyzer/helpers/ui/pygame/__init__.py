87675128ded23b9da0c2ebf8ef1ae128f522375d
Saifeddine <aloui.seifeddine@gmail.com>
2022-01-23 00:44:04 +0100

    upgraded pygame code (ready to be put in a separate library)

--- FaceAnalyzer/helpers/ui/pygame/__init__.py
+++ FaceAnalyzer/helpers/ui/pygame/__init__.py
@@ -6,7 +6,8 @@
     Description :
         User interface helpers
 <================"""
-import io
+from code import interact
+import time
 import pygame
 import cssutils
 from FaceAnalyzer.helpers.geometry.euclidian import is_point_inside_rect
@@ -22,7 +23,22 @@ pygame.font.init()
 
 @dataclass
 class WidgetStyle:
-    """Class for keeping track of an item in inventory."""
+    """Class for keeping track widget styling information.
+        font: The font to use for the text.
+        bg_color: The background color.
+        border_color: The color of the border.
+        text_color: The color of the text.
+        border_size: The size of the border.
+        font_name: The name of the font.
+        font_size: The size of the font.
+        x_margin: The margin on the x-axis.
+        y_margin: The margin on the y-axis.
+        width: The width of the widget.
+        height: The height of the widget.
+        align: The alignment of the text.
+        img: The image to use for the widget.   
+  
+    """
     font : pygame.font.Font = pygame.font.Font('freesansbold.ttf', 14)
     bg_color: tuple = (100,100,100)
     border_color: tuple =(0,0,0)
@@ -32,11 +48,14 @@ class WidgetStyle:
     font_size: int = 24
     x_margin: int = 0
     y_margin: int = 0
+    width: int = None
+    height: int = None
     align:str = 'center'
     img:str = None
 
 
 
+# =============================================== Widget ==========================================
 
 class Widget():
     def __init__(
@@ -45,6 +64,19 @@ class Widget():
                     style:str="widget{background-color:#ffffff;}\n",
                     extra_styles={}
                 ):
+        """
+            Creates a new Widget instance.
+
+rect: 
+within the window.
+style:
+extra_styles: A dictionary of additional CSS style properties
+to be applied to the widget.
+        Args:
+            rect (tuple, optional): A tuple of four numbers, representing the position of the widget. Defaults to [0,0,100,50].
+            style (str, optional):  A string containing the CSS style properties for the widget. Defaults to "widget{background-color:#ffffff;}\n".
+            extra_styles (dict, optional): [description]. Defaults to {}.
+        """
         self.setRect(rect)
         self.visible = True
         self.styles=self.merge_two_dicts({
@@ -52,21 +84,49 @@ class Widget():
         }, extra_styles)
         self.setStyleSheet(style)
 
-    def setPosition(self, pos):
+    def setPosition(self, pos:list):
+        """Sets the position of the rectangle.
+
+        Args:
+            pos (list): The position of the rectangle.
+
+        """
         self.rect[0]=pos[0]
         self.rect[1]=pos[1]
         self.setRect(self.rect)
 
     def setSize(self, size):
+        """This function sets the size of the widget.
+
+        Args:
+            size (list):  The size is given as a tuple of two numbers, the first representing the width and the second representing the height.
+        """
         self.rect[2]=size[0]
         self.rect[3]=size[1]
         self.setRect(self.rect)
 
     def setRect(self, rect):
+        """Creates a new Rectangle object.
+
+        Args:
+            rect (list): the rectangle's coordinates
+        """
         self.rect = rect
         self.rect2 = (rect[0],rect[1],rect[0]+rect[2],rect[1]+rect[3])
 
     def merge_two_dicts(self, x, y):
+        """
+        Merges two dicts, x and y, into a new dict, z. x and y must have the same
+        keys. z is created as a copy of x, and then z's keys and values are updated
+        with those of y.
+
+        Args:
+            x ([type]): [description]
+            y ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         z = x.copy()   # start with keys and values of x
         z.update(y)    # modifies z with keys and values of y
         return z
@@ -87,6 +147,16 @@ class Widget():
                     continue
                 # find property
                 for property in rule.style:
+                    if property.name == 'width':
+                        v = property.value
+                        if v is not None:
+                            style.width = v
+
+                    if property.name == 'height':
+                        v = property.value
+                        if v is not None:
+                            style.width = v
+
                     if property.name == 'color':
                         v = get_color(property.value)
                         if v is not None:
@@ -136,6 +206,27 @@ class Widget():
 
     def handle_events(self, events):
         pass
+
+# =============================================== Timer ==========================================
+class Timer():
+    def __init__(self, callback_fn, intrval_s:float=0.1) -> None:
+        self.callback_fn = callback_fn
+        self.intrval_s = intrval_s
+        self.started = False
+    def start(self):
+        self.started = True
+        self.last_time = time.time()
+    def stop(self):
+        self.started = False    
+    def process(self):
+        dt = time.time() - self.last_time
+        if dt>=self.intrval_s:
+            if self.callback_fn is not None:
+                self.callback_fn()
+            self.last_time = time.time()
+
+# =============================================== Window Manager ==========================================
+
 class WindowManager():
     def __init__(self, window_title:str="", resolution:tuple=(800,600)):
         """Builds a window managaer object
@@ -147,6 +238,23 @@ class WindowManager():
         pygame.display.set_caption(window_title)
         self.widgets = []
         self.events = None
+        self.Running = True
+        self.timers=[]
+        self.menu = None
+
+
+    def build_menu_bar(self):
+        self.menu = MenuBar(self)
+        return self.menu
+
+    def build_timer(self, callback_fn, intrval_ms:int=100):
+        timer = Timer(callback_fn, intrval_ms)
+        self.timers.append(timer)
+        return timer
+
+    def add_timer(self,  timer:Timer):
+        self.timers.append(timer)
+        return timer
 
     def addWidget(self, widget:Widget):
         """Adds a new widget to the widgets list
@@ -163,8 +271,29 @@ class WindowManager():
             if widget.visible:
                 widget.paint(self.screen)
                 widget.handle_events(self.events)
+
+        if self.menu is not None:
+            self.menu.paint(self.screen)
+            self.menu.handle_events(self.events)
         # Update UI
         pygame.display.update()
+        # Check timerds
+        for timer in self.timers:
+            timer.process()
+
+    def loop(self):
+        """[summary]
+        """
+        #  Main loop
+        while self.Running:
+            self.process()
+
+            for event in self.events:
+                if event.type == pygame.QUIT:
+                    print("Done")
+                    Running=False
+            # Update UI
+            pygame.display.update()
 
 class Sprite(Widget):
     def __init__(
@@ -188,6 +317,7 @@ class Sprite(Widget):
 
 
 
+# =============================================== ImageBox ==========================================
 
 class ImageBox(Widget):
     def __init__(
@@ -219,15 +349,17 @@ class ImageBox(Widget):
         if self.surface is not None:
             screen.blit(pygame.transform.scale(self.surface, (self.rect[2], self.rect[3])),(self.rect[0],self.rect[1]))
 
+# =============================================== Label ==========================================
+
 class Label(Widget):
     def __init__(
                     self,
                     text, 
                     rect:tuple=[0,0,100,50], 
-                    style:str="btn.normal{color:red; background-color:#ffffff;}\nbtn.hover{color:red; background-color:#ff0000};",
+                    style:str="label{color:black; background-color:#ffffff;}\n",
                     clicked_event_handler=None
                 ):
-        Widget.__init__(self,rect, style,extra_styles={"label":WidgetStyle()})
+        Widget.__init__(self, rect, style,extra_styles={"label":WidgetStyle()})
         self.text = text
         self.hovered=False
         self.pressed=False
@@ -272,31 +404,40 @@ class Label(Widget):
 
         self.blit_text(style, screen)
 
+# =============================================== Button ==========================================
+
 class Button(Widget):
     def __init__(
                     self,
                     text, 
                     rect:tuple=[0,0,100,50], 
                     style:str="btn.normal{color:red; background-color:#ffffff;}\nbtn.hover{color:red; background-color:#ff0000};\nbtn.pressed{color:red; background-color:#ff0000};",
+                    extra_styles:dict={},
                     is_toggle=False,
-                    clicked_event_handler=None
+                    clicked_event_handler=None,
+                    lost_focus_event_handler=None
                 ):
         Widget.__init__(
                         self,
                         rect,
                         style,
-                        {
+                        self.merge_two_dicts({
                             "btn.normal":WidgetStyle(),
                             "btn.hover":WidgetStyle(),
                             "btn.pressed":WidgetStyle(),
-                        }
+                        }, extra_styles)
                         )
+
+                                
+            
+        
         self.text = text
         self.is_toggle = is_toggle
         self.hovered=False
         self.pressed=False
         self.toggled=False
         self.clicked_event_handler = clicked_event_handler
+        self.lost_focus_event_handler = lost_focus_event_handler
 
 
     def setText(self,text:str)->None:
@@ -351,6 +492,7 @@ class Button(Widget):
             if event.type == pygame.MOUSEMOTION:
                 self.hovered = is_point_inside_rect(event.pos,self.rect2)
             elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.hovered = is_point_inside_rect(event.pos,self.rect2)
                 if self.hovered == True:
                     if self.is_toggle:
                         if not self.toggled:
@@ -360,11 +502,161 @@ class Button(Widget):
                         self.pressed=True
                     if self.clicked_event_handler is not None:
                         self.clicked_event_handler()
+                else:
+                    if self.lost_focus_event_handler is not None:
+                        self.lost_focus_event_handler()
+                
 
             elif event.type == pygame.MOUSEBUTTONUP:
                 if not self.is_toggle:
                     self.pressed=False
                 self.toggled=False
+
+
+
+# =============================================== Menus ==========================================
+# ---------------------------------------------------- Menu Bar -----------------------------------------------------
+
+class MenuBar(Widget):
+    def __init__(
+                self,
+                parent:WindowManager,
+                style: str = "MenuBar{background-color:#ffffff;}\n"
+    ):
+        Widget.__init__(self,style=style, extra_styles={"menu_bar":WidgetStyle()})
+        self.parent = parent
+        self.rect = self.parent
+        self.menus=[]
+        self.setStyleSheet(style)
+
+    def addMenu(self, menu):
+        self.menus.append(menu)
+
+    def paint(self, screen):
+        """Paints the manue
+
+        Args:
+            screen ([type]): The screen on which to blit
+        """
+        style = self.styles["menu_bar"]
+        w, h = pygame.display.get_surface().get_size()
+        if style.height is not None:
+            h = style.height
+        else:
+            h = 20
+        self.setRect([0,0,w,h])
+
+        if style.bg_color is not None:
+            pygame.draw.rect(screen,style.bg_color,self.rect)
+        else:
+            pygame.draw.rect(screen,(0,0,0),self.rect)
+
+        rect_start = 0
+        for menu in self.menus:
+            if menu.visible:
+                rect_start = menu.paint(screen,rect_start)
+
+    def handle_events(self, events):
+        for menu in self.menus:
+            menu.handle_events(events)
+        return super().handle_events(events)                
+
+# ---------------------------------------------------- Menu -----------------------------------------------------
+
+
+class Menu(Button):
+    def __init__(
+                self,
+                parent:MenuBar,
+                caption="",
+                style:str="btn.normal{color:red; background-color:#ffffff;}\nbtn.hover{color:red; background-color:#ff0000};\nbtn.pressed{color:red; background-color:#ff0000};",
+    ):
+        Button.__init__(self, caption,style=style)
+        self.parent = parent
+        self.actions=[]
+        parent.addMenu(self)
+        self.clicked_event_handler      = self.fn_clicked_event_handler
+        self.lost_focus_event_handler   = self.fn_lost_focus_event_handler
+
+    def fn_clicked_event_handler(self):
+        for action in self.actions:
+            action.visible=not action.visible
+
+    def fn_lost_focus_event_handler(self):
+        for action in self.actions:
+            action.visible=False
+
+    def addAction(self, action):
+        action.visible=False
+        self.actions.append(action)
+
+
+    def paint(self, screen, rect_start=0):
+        style = self.styles["widget"]
+        if style.height is not None:
+            h = style.height
+        else:
+            h = 20
+        if style.width is not None:
+            w = style.width
+        else:
+            w = 100
+
+        self.setRect([rect_start,0,w, h])
+        Button.paint(self, screen)
+        y=h
+        for action in self.actions:
+            y = action.preprare(rect_start, y)
+            if action.visible:
+                action.paint(screen)
+
+        return rect_start + w
+
+
+    def handle_events(self, events):
+        for action in self.actions:
+            action.handle_events(events)
+        return super().handle_events(events)     
+
+
+# ---------------------------------------------------- Action -----------------------------------------------------
+
+
+class Action(Button):
+    def __init__(
+                self,
+                parent:Menu,
+                caption="",
+                style:str="btn.normal{color:red; background-color:#ffffff;}\nbtn.hover{color:red; background-color:#ff0000};\nbtn.pressed{color:red; background-color:#ff0000};",
+    ):
+        Button.__init__(self, caption,style=style)
+        self.parent = parent
+        self.actions=[]
+        parent.addAction(self)
+
+    def preprare(self, rect_xstart=0, rect_ystart=0):
+        style = self.styles["widget"]
+        if style.height is not None:
+            h = style.height
+        else:
+            h = 20
+        if style.width is not None:
+            w = style.width
+        else:
+            w = 100
+
+        self.setRect([rect_xstart,rect_ystart,w, h])        
+        return rect_ystart + w
+
+    def paint(self, screen):
+        style = self.styles["widget"]
+
+        Button.paint(self, screen)
+
+
+
+
+# =============================================== ProgressBar ==========================================
 
 class ProgressBar(Widget):
     def __init__(
--- examples/Pygame/face_box/face_box.py
+++ examples/Pygame/face_box/face_box.py
@@ -13,7 +13,7 @@ from FaceAnalyzer import FaceAnalyzer, Face,  DrawingSpec, buildCameraMatrix
 from FaceAnalyzer.helpers.geometry.orientation import faceOrientation2Euler
 from FaceAnalyzer.helpers.geometry.euclidian import get_z_line_equation, get_plane_infos, get_plane_line_intersection, region_3d_2_region_2d, is_point_inside_region
 from FaceAnalyzer.helpers.ui.pillow import pilDrawCross, pilShowErrorEllipse, pilOverlayImageWirthAlpha
-from FaceAnalyzer.helpers.ui.pygame import WindowManager, ImageBox
+from FaceAnalyzer.helpers.ui.pygame import Action, Label, WindowManager, MenuBar, Menu, Timer, ImageBox
 
 from FaceAnalyzer.helpers.estimation import KalmanFilter
 import numpy as np
@@ -45,37 +45,69 @@ box_colors=[
 
 # ===== Build pygame window and populate with widgets ===================
 pygame.init()
-wm = WindowManager("Face box", (width,height))
-feedImage = ImageBox(rect=[0,0,width,height])
-wm.addWidget(feedImage)
+
+
+# ===================
+# Project : StopMotionMaer
+# Author : ParisNeo  (Saifeddine ALOUI)
+# Description :
+# A free software to create stop motion videos based on moviepy library
+# ===================
+
+import pygame
+from moviepy.editor import VideoFileClip
+
+# ===== Build pygame window and populate with widgets ===================
+pygame.init()
+class MainWindow(WindowManager):
+    def __init__(self):
+        WindowManager.__init__(self, "Face box", (width,height))
+        self.mn_bar = self.build_menu_bar()
+        self.file = Menu(self.mn_bar,"File")
+        quit = Action(self.file,"Quit")
+        quit.clicked_event_handler = self.fn_quit
+        self.lbl_fps = Label("FPS",rect=[0,20,100,20],style="")
+        self.feedImage = ImageBox(rect=[0,20,width,height])
+        self.addWidget(self.feedImage)
+        self.addWidget(self.lbl_fps)
+
+        self.motion_stuf = self.build_timer(self.do_stuf,0.001)
+        self.motion_stuf.start()
+        self.curr_frame_time = time.time()
+        self.prev_frame_time = self.curr_frame_time
+
+    def do_stuf(self):
+        success, image = cap.read()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)#
+        # Process the image to extract faces and draw the masks on the face in the image
+        fa.process(image)
+
+        if fa.nb_faces>0:
+            for i in range(fa.nb_faces):
+                face = fa.faces[i]
+                # Get head position and orientation compared to the reference pose (here the first frame will define the orientation 0,0,0)
+                pos, ori = face.get_head_posture()
+                if pos is not None:
+                    yaw, pitch, roll = faceOrientation2Euler(ori, degrees=True)
+                    face.draw_oriented_bounding_box(image, color=box_colors[i%3], thickness=1)
+                    face.draw_reference_frame(image, pos, ori, origin=face.get_landmark_pos(Face.nose_tip_index))
+
+        # Process fps
+        self.curr_frame_time = time.time()
+        dt = self.curr_frame_time-self.prev_frame_time
+        self.prev_frame_time = self.curr_frame_time
+        self.fps = 1/dt
+        self.lbl_fps.setText(f"FPS : {self.fps:0.2f}")
+
+        self.feedImage.setImage(image)
+
+    def fn_quit(self):
+        self.Running=False
+    
 # =======================================================================
-Running = True
 
-#  Main loop
-while Running:
-    success, image = cap.read()
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)#
-    # Process the image to extract faces and draw the masks on the face in the image
-    fa.process(image)
-
-    if fa.nb_faces>0:
-        for i in range(fa.nb_faces):
-            face = fa.faces[i]
-            # Get head position and orientation compared to the reference pose (here the first frame will define the orientation 0,0,0)
-            pos, ori = face.get_head_posture()
-            if pos is not None:
-                yaw, pitch, roll = faceOrientation2Euler(ori, degrees=True)
-                face.draw_oriented_bounding_box(image, color=box_colors[i%3], thickness=1)
-                face.draw_reference_frame(image, pos, ori, origin=face.get_landmark_pos(Face.nose_tip_index))
-
-
-    image = np.swapaxes(image,0,1)#cv2.flip(, 1)
-    feedImage.setImage(image)
-    wm.process()
-
-    for event in wm.events:
-        if event.type == pygame.QUIT:
-            print("Done")
-            Running=False
-    # Update UI
-    pygame.display.update()
+#clip = VideoFileClip(r'C:\Users\aloui\Videos\test\Terminator.mp4')
+#clip.preview()
+if __name__=="__main__":
+    mw = MainWindow()
+    mw.loop()
